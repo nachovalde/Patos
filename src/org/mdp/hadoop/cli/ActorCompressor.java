4dfb30330a13c53062c106a34a584bb25de64146
joromero <joromerom@gmail.com>
2015-07-31 12:26:53 -0300

    Compresor de actores

---
+++ src/org/mdp/hadoop/cli/ActorCompressor.java
@@ -0,0 +1,71 @@
+package org.mdp.hadoop.cli;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.omg.PortableInterceptor.ACTIVE;
+
+public class ActorCompressor {
+	static final String SPLIT_REGEX = "\t";
+	static final String BACON = "Bacon, Kevin (I)";
+	private String dict, searched;
+	public ActorCompressor(String dictPath, String searched){
+		dict = dictPath;
+		this.searched = searched;
+	}
+
+	public int compress(String src, String dest) throws IOException{
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		int i=1;
+		map.put(searched, 0);
+		BufferedReader br = new BufferedReader(new FileReader(new File(src)));
+		BufferedWriter out = new BufferedWriter(new FileWriter(new File(dest)));
+		BufferedWriter d = new BufferedWriter(new FileWriter(new File(dict)));
+		String[] L;
+		for(String line = br.readLine(); line!=null; line=br.readLine()){
+			System.out.println(line);
+			L = line.split(SPLIT_REGEX);
+			Integer val = map.get(L[0]);
+			if(val==null){
+				map.put(L[0], i);
+				val = i++;
+			}
+			out.write(val+"\t");
+			L = line.split(SPLIT_REGEX);
+			val = map.get(L[1]);
+			if(val==null){
+				map.put(L[1], i);
+				val = i++;
+			}
+			out.write(val+"\n");
+		}
+		br.close();
+		out.close();
+		for(String val:map.keySet()){
+			d.write(map.get(val)+"\t"+val+"\n");
+		}
+		d.close();
+		return i;
+	}
+
+	public static void main(String[] args) throws IOException {
+		if (args.length != 3) {
+			System.err.println("Usage: WordCount <in> <dict> <out>");
+			System.exit(2);
+		}
+		String inputLocation = args[0];
+		String dictLocation = args[1];
+		String outputLocation = args[2];
+		ActorCompressor ac = new ActorCompressor(dictLocation, ActorCompressor.BACON);
+		ac.compress(inputLocation, outputLocation);
+	}
+
+}
